1c67bcf092d62c48079229f6efefd135601a2725
rerades <rerades@siete3.com>
2019-08-19 11:36:08 +0200

    adding webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './src',
+    overlay: true,
+    disableHostCheck: true,
+  },
+  stats: {
+    colors: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'NEW SEED',
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
